dab59bec1616bc2edd3f5dd22ca2cabaf826ac0b
Adauto <adautofabiano.silva@gmail.com>
2021-04-13 18:52:13 -0300

    Commit inicial

---
+++ src/main/java/com/adasi/SpringReactApplication.java
@@ -0,0 +1,16 @@
+package com.adasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication()
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringReactApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/adasi/student/StudentsController.java
@@ -0,0 +1,31 @@
+package com.adasi.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("teste")
+public class StudentsController {
+
+    @GetMapping()
+    public List<Student> getAllStudents(){
+        return List.of(new Student(UUID.randomUUID(),"Adauto","Fabiano e silva","adasdi@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(),"Maria","Aparecida e silva","Maria@gmail.com", Student.Gender.FEMALE),
+                new Student(UUID.randomUUID(),"João","Victor e silva","JV@gmail.com", Student.Gender.MALE));
+
+//        List<Student> l = new ArrayList<>();
+//        l.add( );
+//        l.add( );
+//        return l;
+
+
+
+    }
+
+
+}
